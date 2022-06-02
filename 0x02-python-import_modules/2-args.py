3f6f3d230004fd4abab7de1adcfe95821d77e7ae
Kumenit <86913518+Kumenit@users.noreply.github.com>
2022-06-02 07:22:03 -0700

    p2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+from sys import argv
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    i = 1
+    args = len(argv) - 1
+
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
+    print(f"{'.' if args == 0 else ':'}")
+    while (i <= args):
+        print(f"{i}: {argv[i]}")
+        i += 1
